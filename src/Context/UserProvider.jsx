9d30ff4f05096314d232373b1c3d11803570f396
mehan <mehandihassan1@gmail.com>
2023-08-26 16:01:15 +0530

    Download the zip file from GitHub. Go to the folder where the file was downloaded and extract it. Open the extracted files in Visual Studio Code or any other code editor platform. In Visual Studio Code, open the terminal and run the command "npm i" to install the required dependencies. After the dependencies are installed, run the command "npm start" on the terminal. Open your web browser and type "localhost:3000" to access the site, or it may automatically open. Explore the site

---
+++ src/Context/UserProvider.jsx
@@ -0,0 +1,41 @@
+// Import necessary dependencies
+import axios from 'axios';
+import React, { createContext, useEffect, useState } from 'react'
+
+// Create a userContext object
+export const userContext = createContext();
+// Get user data from local storage
+const dataArr = JSON.parse(localStorage.getItem("userData"))
+
+// Create a UserProvider component
+const UserProvider = ({ children }) => {
+// Set initial state for users array
+
+const [users, setUsers] = useState([]);
+
+// Function to get user data from API endpoint
+const getData = async () => {
+try {
+const res = await axios.get('https://panorbit.in/api/users.json');
+const userData = res.data.users;
+setUsers(userData);
+} catch (error) {
+console.error(error);
+}
+};
+
+// Call getData function on component mount using useEffect hook
+useEffect(() => {
+getData();
+}, []);
+
+// Pass users state, getData function and user data from local storage state as values to userContext provider
+return (
+<userContext.Provider value={{ users, getData, dataArr }}>
+{children}
+</userContext.Provider>
+)
+}
+
+// Export UserProvider component
+export default UserProvider;
---
+++ src/routes/ToDo.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Chat from "../Components/ChatPage";
+import Navbar from "../Components/Navbar";
+import SideNavbar from "../Components/SideNavbar";
+import { userContext } from "../Context/UserProvider";
+
+const ToDo = () => {
+
+  
+  return (
+    <div className="">
+      <div className=" flex ">
+        <div className=" w-[24%]">
+          <SideNavbar />
+        </div>
+        <div className="py-12 px-8 w-[76%]">
+          <Navbar />
+          <hr/>
+            <div className="mt-4 p-8">
+              <div className="text-center font-bold text-gray-400 text-[5rem] opacity-25 p-40">
+                Coming Soon
+              </div>
+            </div>
+        </div>
+      </div>
+      <Chat />
+    </div>
+  );
+};
+
+export default ToDo;
